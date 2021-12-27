b5b1734849697c921c7ad58c37f0960d6bcd4337
Leonardo Vaz <leonardovazlourenco@gmail.com>
2021-12-27 18:23:33 -0300

    up
    up

---
+++ app/src/main/java/br/com/dio/todolist/ui/MainActivity.kt
@@ -0,0 +1,30 @@
+package br.com.dio.todolist.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.dio.todolist.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val adapter by lazy { TaskListAdapter() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rvTasks.adapter = adapter
+
+        insertListenrs()
+    }
+
+    private fun insertListenrs()
+    {
+        binding.fab.setOnClickListener()
+        {
+            startActivity(Intent(this, AddTaskActivity::class.java))
+        }
+    }
+}
